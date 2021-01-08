c3e2e8c90f3c939fbb4402877ee0248b7ca2aa00
d-w-moore <dmoore@renci.org>
2021-01-08 07:56:08 -0500

    optional deferred creation of ICAT

--- docker_iRODS_build/start_postgresql_and_irods.sh
+++ docker_iRODS_build/start_postgresql_and_irods.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 service postgresql start
-
-x=${DB_WAIT_SEC:-15}
+x=${DB_WAIT_SEC:-20}
 while [ $x -ge 0 ] && { ! $SUDO su - postgres -c "psql -c '\l' >/dev/null 2>&1" || x=""; }
 do
   [ -z "$x" ] && break
   echo >&2 "$((x--)) secs til database timeout"; sleep 1
 done
 [ -z "$x" ] || { echo >&2 "Error -- database didn't start" ; exit 1; }
-
-id -u irods >/dev/null 2>&1 || ~/ubuntu_irods_installer/install.sh 5
+if ! id -u irods >/dev/null 2>&1 ; then
+    if [ -f "{ICAT_DEFERRED_CREATEDB}" ] ; then
+        ~/ubuntu_irods_installer/install.sh --w=create-db 0
+        rm -f "{ICAT_DEFERRED_CREATEDB}"
+    fi
+    ~/ubuntu_irods_installer/install.sh 5
+else
+    su - irods -c '~/irodsctl start'
+fi
